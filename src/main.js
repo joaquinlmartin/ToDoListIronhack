a5864d3d93da0fdcea20a33626457afd20b4eb16
unknown <joaquinlmartin@gmail.com>
2022-09-30 17:08:08 +0200

    add files

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import './static/main.css';
 
 createApp(App).use(router).use(createPinia()).mount('#app');
