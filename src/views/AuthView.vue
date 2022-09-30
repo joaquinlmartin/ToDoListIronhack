3a576981b4439e4ddebbbebd4eebf596fa79533b
unknown <joaquinlmartin@gmail.com>
2022-09-30 15:46:28 +0200

    signup

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,9 +1,26 @@
 <template>
     <h1>Esta es la vista del Sign up</h1>
+    <button @click="singUp">Sign Up</button>
 </template>
 
 <script>
+import { mapState, mapActions } from 'pinia';
+import userStore from '@/store/user';
+
 export default {
   name: 'Authview',
+  computed: {
+    ...mapState(userStore, ['user']),
+  },
+  methods: {
+    ...mapActions(userStore, ['signUp']),
+    signUp() {
+      const userData = {
+        email: 'joaquinlmartin@gmail.com',
+        password: 'prueba1234',
+      };
+      this.signUp(userData);
+    },
+  },
 };
 </script>
