f94d9bdefd06f871ce1a8befce5eea479c6ee671
unknown <joaquinlmartin@gmail.com>
2022-09-30 03:21:38 +0200

    supabase and configs added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/store/task.js
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia';
+import supabase from '../supabase/index';
+
+export default defineStore('tasks', {
+  state: () => ({
+    tasks: null,
+  }),
+  actions: {
+    async fetchTasks() {
+      const { data: tasks } = await supabase
+        .from('tasks')
+        .select('*')
+        .order('id', { ascending: false });
+      this.tasks = tasks;
+    },
+  },
+});
